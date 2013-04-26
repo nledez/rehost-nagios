e2f0e99063dd688c550a24c09a9fd24c5d34f863
Nicolas Ledez <github@ledez.net>
2013-04-26 08:50:51 +0200

    Refactor expect(chef_run) to runner

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -3,18 +3,20 @@ require 'chefspec'
 describe 'rehost-nagios::default' do
   let(:chef_run) { ChefSpec::ChefRunner.new.converge 'rehost-nagios::default' }
   it 'Deploy nagios-nrpe-server' do
+    runner = expect(chef_run)
+
     [ "nagios-nrpe-server", "nagios-plugins", "nagios-plugins-basic", "nagios-plugins-standard" ].each do |p|
-      expect(chef_run).to install_package p
+      runner.to install_package p
     end
 
-    expect(chef_run).to set_service_to_start_on_boot 'nagios-nrpe-server'
-    expect(chef_run).to start_service 'nagios-nrpe-server'
+    runner.to set_service_to_start_on_boot 'nagios-nrpe-server'
+    runner.to start_service 'nagios-nrpe-server'
 
-    expect(chef_run).to create_directory "/usr/local/lib/nagios/plugins"
+    runner.to create_directory "/usr/local/lib/nagios/plugins"
 
-    expect(chef_run).to create_file "/etc/nagios/nrpe.d/syslog-ng.cfg"
+    #runner.template("/etc/nagios/nrpe.d/syslog-ng.cfg").should exist
   #  #[ "syslog-ng.cfg", "system.cfg", "linux.cfg" ].each do |f|
-  #    #expect(chef_run).to create_file "/etc/nagios/nrpe.d/#{f}"
+  #    #runner.to create_file "/etc/nagios/nrpe.d/#{f}"
   #    #file = chef_run.file("/etc/nagios/nrpe.d/#{f}")
   #    #expect(file).to be_owned_by('root', 'root')
   end
