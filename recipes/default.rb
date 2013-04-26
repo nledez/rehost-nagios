0e19af8af438c63f5456b3014b480cd8df774b8d
Nicolas Ledez <github@ledez.net>
2013-04-26 08:39:21 +0200

    Create recursive directory & config file But test does'nt work for files

--- recipes/default.rb
+++ recipes/default.rb
@@ -18,6 +18,7 @@ service node['rehost-nagios']['nrpe-service'] do
 end
 
 directory node['rehost-nagios']['script-dir'] do
+  recursive true
   owner "root"
   group "root"
   mode "0755"
@@ -28,12 +29,12 @@ end
   #cookbook_file "#{node['rehost-nagios']['config-dir']}/#{f}" do
     #source "conf/#{f}"
 #[ "syslog-ng.cfg", "system.cfg", "linux.cfg" ].each do |f|
-cookbook_file "syslog-ng.cfg" do
+cookbook_file "/etc/nagios/nrpe.d/syslog-ng.cfg" do
   path "/etc/nagios/nrpe.d/syslog-ng.cfg"
   source "conf/syslog-ng.cfg"
-  #mode '0644'
-  #owner 'root'
-  #group 'root'
+  mode '0644'
+  owner 'root'
+  group 'root'
   action :create
 end
 #end
--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -2,27 +2,20 @@ require 'chefspec'
 
 describe 'rehost-nagios::default' do
   let(:chef_run) { ChefSpec::ChefRunner.new.converge 'rehost-nagios::default' }
-  it 'installs packages' do
+  it 'Deploy nagios-nrpe-server' do
     [ "nagios-nrpe-server", "nagios-plugins", "nagios-plugins-basic", "nagios-plugins-standard" ].each do |p|
       expect(chef_run).to install_package p
     end
-  end
 
-  it 'start & enable nagios-nrpe-server' do
     expect(chef_run).to set_service_to_start_on_boot 'nagios-nrpe-server'
     expect(chef_run).to start_service 'nagios-nrpe-server'
-  end
 
-  it 'Create folders' do
     expect(chef_run).to create_directory "/usr/local/lib/nagios/plugins"
-  end
 
-  it 'Create files config' do
     expect(chef_run).to create_file "/etc/nagios/nrpe.d/syslog-ng.cfg"
-    #[ "syslog-ng.cfg", "system.cfg", "linux.cfg" ].each do |f|
-      #expect(chef_run).to create_file "/etc/nagios/nrpe.d/#{f}"
-      #file = chef_run.file("/etc/nagios/nrpe.d/#{f}")
-      #expect(file).to be_owned_by('root', 'root')
-    #end
+  #  #[ "syslog-ng.cfg", "system.cfg", "linux.cfg" ].each do |f|
+  #    #expect(chef_run).to create_file "/etc/nagios/nrpe.d/#{f}"
+  #    #file = chef_run.file("/etc/nagios/nrpe.d/#{f}")
+  #    #expect(file).to be_owned_by('root', 'root')
   end
 end
