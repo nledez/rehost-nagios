3fe2c6070dac1d4b966081f03c6324d93ad0d754
Nicolas Ledez <github@ledez.net>
2013-04-27 17:32:18 +0200

    Add imap recipe

---
+++ spec/imap_spec.rb
@@ -0,0 +1,14 @@
+require 'chefspec'
+
+describe 'rehost-nagios::imap' do
+  let(:chef_run) { ChefSpec::ChefRunner.new.converge 'rehost-nagios::imap' }
+  it 'Add imap config files' do
+    runner = expect(chef_run)
+
+    [ "imap.cfg" ].each do |f|
+      runner.to create_cookbook_file "/etc/nagios/nrpe.d/#{f}"
+      file = chef_run.cookbook_file("/etc/nagios/nrpe.d/#{f}")
+      expect(file).to be_owned_by('root', 'root')
+    end
+  end
+end
