cbb7fab209e8111de07a4e37606b67af05d426b0
Nicolas Ledez <github@ledez.net>
2013-09-30 12:32:37 +0200

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,4 @@ maintainer_email 'nicolas@ledez.net'
 license          'All rights reserved'
 description      'Installs/Configures nagios nrpe 4 rehost infra'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.9'
+version          '1.0.10'
