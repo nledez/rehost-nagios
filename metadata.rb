5d45483ff11569365c2d297cbbb5947cdaa475a8
Nicolas Ledez <github@ledez.net>
2014-01-10 23:15:59 +0100

    Fix maintainer & Licence. But real opensource one

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'rehost-nagios'
-maintainer       'ReHost'
-maintainer_email 'nicolas@ledez.net'
-license          'All rights reserved'
+maintainer       'ReHost - Nicolas Ledez'
+maintainer_email 'chef-cookbook@ledez.net'
+license          'Apache v2.0'
 description      'Installs/Configures nagios nrpe 4 rehost infra'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.0.11'
